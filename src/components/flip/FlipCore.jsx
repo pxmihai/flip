2651d53c7f3ac34d20b0c9c7a7f99f0128592476
pxmihai <pxmihai@yahoo.com>
2021-02-01 13:15:41 +0200

    added icons, position

--- src/components/flip/FlipCore.jsx
+++ src/components/flip/FlipCore.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import styled from 'styled-components'
 import img1 from './img/bg-stars.svg'
-
 import './css/flip.css'
 
 const StyledBackground = styled.div`
   background-image: url(${img1});
   background-color: hsl(235, 16%, 14%);
   position: relative;
-
   width: 100%;
   height: 100%;
-
 `
-
 
 export const FlipCore = () => {
     return (
         <StyledBackground>
 
-            <div className="cover">
-                <div className="aa">WE'RE LAUNCHING SOON</div>
-                <div className="bb"> </div>
-                <div className="cc">
+            <div className="wrapper">
+                <div className="top">WE'RE LAUNCHING SOON</div>
+                <div className="countdown"> </div>
+                <div className="bottom">
+                    <i className="fab fa-facebook-square"></i>
+                    <i className="fab fa-pinterest"></i>
+                    <i className="fab fa-instagram"></i>
+
 
                 </div>
             </div>
