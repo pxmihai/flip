35290aedf9c1d03885402f379d8c5a84beab8b68
pxmihai <pxmihai@yahoo.com>
2021-02-04 15:42:10 +0200

    - todo :     responsive     colors     centering

--- src/components/flip/FlipCore.jsx
+++ src/components/flip/FlipCore.jsx
@@ -11,23 +11,23 @@ const StyledBackground = styled.div`
   height: 100%;
 `
 
-const StyledDaysID = styled.p`
-  background-color: orange;
-
-  //    testing purposes
-`
+// const StyledID = styled.p`
+//   background-color: orange;
+//
+//   //    testing purposes
+// `
 
 export const FlipCore = () => {
     let [days, setDays] = useState(0);
     let [hours, setHours] = useState(0);
     let [minutes, setMinutes] = useState(0);
     let [seconds, setSeconds] = useState(0);
-
-    let launchDate = new Date("July 10, 2021 16:17:18").getTime();
+    let launchDate = new Date("Mar 1, 2021 16:20:00").getTime();
     let now = new Date().getTime();
     let eta = launchDate - now;
     let day = 1000 * 60 * 60 * 24;
     useEffect(() => {
+        //https://medium.com/create-a-clocking-in-system-on-react/create-a-react-app-displaying-the-current-date-and-time-using-hooks-21d946971556
         let timer = setInterval(() => {
                 setDays(days = Math.floor(eta / day));
                 setHours(hours = Math.floor(eta % ((1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
@@ -47,19 +47,55 @@ export const FlipCore = () => {
             <div className="wrapper">
                 <div className="top">WE'RE LAUNCHING SOON</div>
                 <div className="countdown">
-                    <div className="flip_label">
-                        <div>
-                            <p id="days" className="value">{days}</p>
+                    <div className="flip">
+
+                        <div className="value">
+                            <div id="leftC"></div>
+                            {days}
+                            <div id="rightC"></div>
                         </div>
-                        <div id="mess">
-                            <StyledDaysID>
-                                <p id="days">days</p>
-                            </StyledDaysID>
+
+                        <div className="unit">
+                            <p>DAYS</p>
                         </div>
                     </div>
-                    <div className="flip_label">{hours}hours</div>
-                    <div className="flip_label">{minutes}minutes</div>
-                    <div className="flip_label">{seconds}seconds</div>
+                    <div className="flip">
+
+                        <div className="value">
+                            <div id="leftC"></div>
+                            {hours}
+                            <div id="rightC"></div>
+                        </div>
+
+                        <div className="unit">
+                            <p>HOURS</p>
+                        </div>
+                    </div>
+                    <div className="flip">
+
+                        <div className="value">
+                            <div id="leftC"></div>
+                            {minutes}
+                            <div id="rightC"></div>
+                        </div>
+
+                        <div className="unit">
+                            <p>MINUTES</p>
+                        </div>
+                    </div>
+                    <div className="flip">
+
+                            <div  className="value">
+                                <div id="leftC"></div>
+                                {seconds}
+                                <div id="rightC"></div>
+                            </div>
+
+                        <div class="unit">
+                                <p>SECONDS</p>
+                        </div>
+                    </div>
+
                 </div>
                 <div className="bottom">
                     <i className="fab fa-facebook-square"></i>
