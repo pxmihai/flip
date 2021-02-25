a14971017df405962e5b8dd68a35c87245bbb308
pxmihai <pxmihai@yahoo.com>
2021-02-25 13:22:33 +0200

    - idk

--- src/components/header/HeaderCore.js
+++ src/components/header/HeaderCore.js
@@ -8,14 +8,27 @@ const StyledHeader = styled.div`
   align-items: center;
   background-color: orange;
 `
+const StyledBox = styled.div`
+  display: flex;
+  margin:10px;
+  background-color:tomato;
+`
 
 export const HeaderCore = () => {
     // data goes here
 
     const [postId, setPostId] = useState(null);
+    const [startValue, endValue] = useState(0);
+    const [starBit, moveBit] = useState({a: 1, b: 2, c: "potato"})
+    const [potato, tomato] = useState("words");
+
 
     useEffect(() => {
         // PUT request using fetch inside useEffect React hook
+        endValue("eraser 991");
+        moveBit({c:"erase 99c"});
+        moveBit({a:"erase 99a"});
+        tomato("erase 993")
         const requestOptions = {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
@@ -29,12 +42,20 @@ export const HeaderCore = () => {
     }, []);
 
     return (
+        <div>
+            <StyledHeader>
 
-        <StyledHeader>
-            Value : {postId}
+                    <StyledBox>Value : {postId}</StyledBox>
+                    <StyledBox>Value : {startValue}</StyledBox>
+                    <StyledBox>Value : {starBit.a}</StyledBox>
+                    <StyledBox>Value : {potato}</StyledBox>
+
+
+
             </StyledHeader>
 
-    );
+
+        </div>);
 }
 
 // https://jasonwatmore.com/post/2020/11/02/react-fetch-http-put-request-examples
